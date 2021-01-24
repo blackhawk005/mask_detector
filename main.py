004eed3bbe961270aab1669846cdfe3ff9a5332d
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-01-25 02:24:08 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,114 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import uuid
+from mpyg321.mpyg321 import MPyg321Player
+import time
+import cv2
+import os
+import warnings
+
+warnings.filterwarnings('ignore')
+bool = 1
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+                                 (104.0, 177.0, 123.0))
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+    # print(detections.shape)
+
+    faces = []
+    locs = []
+    preds = []
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype(int)
+
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+    if len(faces) > 0:
+        faces = np.array(faces, dtype='float32')
+        preds = maskNet.predict(faces, batch_size=32)
+
+    return (locs, preds)
+
+
+prototxtPath = r"face_detector/deploy.prototxt"
+weightsPath = r"face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+maskNet = load_model('mask_detector.model')
+print('starting video stream')
+
+vs = VideoStream(src=0).start()
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    # detect faces in the frame and determine if they are wearing a
+    # face mask or not
+    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+    # loop over the detected face locations and their corresponding
+    # locations
+    for (box, pred) in zip(locs, preds):
+        # unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+
+        # determine the class label and color we'll use to draw
+        # the bounding box and text
+        label = ''
+        if mask > withoutMask:
+            label = 'Mask'
+            bool = 1
+        elif withoutMask > mask:
+            label = 'No Mask'
+            imgname = './Images/No_Mask/{}.jpg'.format(str(uuid.uuid1()))
+            if bool == 1:
+                cv2.imwrite(imgname, frame)
+                bool = 0
+            player = MPyg321Player()
+            player.play_song("beep.mp3")
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+        # display the label and bounding box rectangle on the output
+        # frame
+        cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        bool = 1
+        break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
